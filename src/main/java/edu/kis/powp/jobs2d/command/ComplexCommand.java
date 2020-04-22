cfa69a6529bffed2f59d4aa3cdd6d9cd91a94940
Leonard Wojtczak <medyk170@gmail.com>
2020-04-22 12:39:31 +0200

    3.1.1 Add basic clases and interface

---
+++ src/main/java/edu/kis/powp/jobs2d/command/ComplexCommand.java
@@ -0,0 +1,4 @@
+package edu.kis.powp.jobs2d.command;
+
+public class ComplexCommand {
+}
---
+++ src/main/java/edu/kis/powp/jobs2d/command/OperateToCommand.java
@@ -0,0 +1,20 @@
+package edu.kis.powp.jobs2d.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class OperateToCommand implements DriverCommand {
+
+    private int x;
+    private int y;
+
+    public OperateToCommand(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void execute(Job2dDriver job2dDriver) {
+        job2dDriver.operateTo(this.x, this.y);
+    }
+
+}
